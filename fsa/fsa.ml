c4fa3131324ac741c0c98ccdcea9f200f992b6ae
minishrink <mathur_akanksha@hotmail.com>
2017-11-15 14:45:27 +0000

    Move execution helpers into one main function
    Signed-off-by: minishrink <mathur_akanksha@hotmail.com>

--- fsa/fsa.ml
+++ fsa/fsa.ml
@@ -31,37 +31,38 @@ let add_transition fsa ltr src dst =
 
 (*--- execution logic ---*)
 
+let process_string fsa str =
+
 (* Search automaton Hashtbl for next transition on given symbol
    raises Illegal_transition on invalid input or Not_found
    automaton -> symbol -> state -> state *)
-let get_next_state fsa ltr current_state =
-  try
-    match ltr, current_state with
-    | Sym x, St y -> 
-      let transitions = Hashtbl.find_all fsa.transitions current_state in
-      List.assoc ltr transitions
-  with
-  | Not_found -> raise Illegal_transition
-  | _ -> raise Invalid_input
+  let get_next_state fsa ltr current_state =
+    try
+      match ltr, current_state with
+      | Sym x, St y -> 
+        let transitions = Hashtbl.find_all fsa.transitions current_state in
+        List.assoc ltr transitions
+    with
+    | Not_found -> raise Illegal_transition
+    | _ -> raise Invalid_input
+  in
+  (* currently only used once, may be helpful in future *)
+  let is_final fsa state = List.mem state fsa.final in
 
-let is_final fsa state = List.mem state fsa.final
+  (* transform string input into char list for easier traversal *)
+  let str_to_char_lst str =
+    let len = String.length str in
+    let rec chrl x =
+      if x < len
+      then str.[x]::(chrl (x+1))
+      else []
+    in chrl 0
+  in
 
-(* transform string input into char list for easier traversal *)
-let str_to_char_lst str =
-  let len = String.length str in
-  let rec chrl x =
-    if x < len
-    then str.[x]::(chrl (x+1))
-    else []
-  in chrl 0
+  (* input string -> char list -> Sym list *)
+  let lst = str |> str_to_char_lst |> make_alphabet in
 
-(* input string -> char list -> Sym list *)
-let format_input str =
-  str |> str_to_char_lst |> make_alphabet
-
-(* pass in string, return result *)
-let process_string fsa str =
-  let lst = format_input str in
+  (* pass in string, return result *)
   let rec run st = function
     | [] -> if is_final fsa st
       then Accepted
