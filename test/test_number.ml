9cfc2411de1d1488bfe030ec10997b86c7d99404
minishrink <minishrink@github.com>
2018-04-24 13:40:47 +0100

    Update tests with refactoring changes, make more modular
    Signed-off-by: minishrink <minishrink@github.com>

--- test/test_number.ml
+++ test/test_number.ml
@@ -1,24 +1,52 @@
 
 module DFA = Number.DFA
-module N =Number
+module N = Number
 
 module Testable = struct
-  let result_token = Alcotest.testable (Fmt.of_to_string DFA.result_string) (=)
+  let result_token = Alcotest.testable (Fmt.of_to_string N.Num.token_string) (=)
+  let result = Alcotest.testable (Fmt.of_to_string DFA.result_string) (=)
+  let final_state = Alcotest.testable (Fmt.of_to_string DFA.print_state) (=)
 end
 
-let check_accepted value =
+let test_with_debugging test =
+  try
+    ignore (test ())
+  with
+  | DFA.Automaton_failure e ->
+    Printf.printf "\nAUTOMATON_FAILURE [ %s ]\n" (DFA.automaton_error_string e)
+
+let check_accepted expected actual =
   Alcotest.check Testable.result_token
     "Check for acceptance"
-    DFA.Accepted
-    (N.get_result value)
+    expected actual
+
+let check_rejected actual =
+  Alcotest.check Testable.result
+    "Check for rejection"
+    DFA.Rejected actual
+
+let check_int input   = check_accepted N.Num.(Int input)
+let check_float input = check_accepted N.Num.(Float input)
+
 
 let test_accepted_values () =
-  List.iter check_accepted
-    [ "0.9"
-    ; "7"
-    ; "2352.2352"
+  List.iter test_with_debugging
+    [ (fun () -> check_float "0.9")
+    ; (fun () -> check_int "7")
+    ; (fun () -> check_float "2352.2352")
+    ]
+
+let test_rejected_values () =
+  List.iter
+    (fun input -> test_with_debugging
+        (fun () -> input |> DFA.get_result |> check_rejected)
+    )
+    [ "."
+    ; "92."
+    ; ".325"
     ]
 
 let test =
   [ "test_accepted_values", `Quick, test_accepted_values
+  ; "test_rejected_values", `Quick, test_rejected_values
   ]
